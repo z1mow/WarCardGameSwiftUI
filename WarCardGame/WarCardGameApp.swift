3b5c03b5d95a6aa4716ca7b9cc6d9aa54fbab748
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-03-03 10:32:15 +0300

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 3.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
